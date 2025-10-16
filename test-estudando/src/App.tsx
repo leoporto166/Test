a4f101343a49b44d61a97979e1f2b1944d2fe662
leoporto166 <leonardo.porto166@gmail.com>
2025-10-15 21:44:12 -0300

    tests

---
+++ test-estudando/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Leonardo</h1>
+      <p>Teste</p>
+    </>
+  )
+}
+
+export default App
---
+++ test-estudando/src/app.test.tsx
@@ -0,0 +1,9 @@
+
+//Criar um bloco que agrupo varios testese relacionados
+describe("First test app component", () => {
+    it("should add 1 + 2 to equal 3", () => {
+        expect(1).toBe(1);
+    })
+})
+
+export default {}
