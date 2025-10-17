2c01da996c3c4046c55fdd35f5ecca42345dffd5
leoporto166 <leonardo.porto166@gmail.com>
2025-10-16 22:01:56 -0300

    aplicando tests

--- test-estudando/src/App.tsx
+++ test-estudando/src/App.tsx
@@ -1,12 +1,19 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [message, setMessage] = useState("Bem vindo ao projeto")
+
   return (
     <>
-      <h1>Leonardo</h1>
-      <p>Teste</p>
+      <h1>Projeto Test</h1>
+      <p>{message}</p>
+
+      <button onClick={() => setMessage("Estudando Test")}>
+        Alterar
+      </button>
     </>
   )
 }
--- test-estudando/src/app.test.tsx
+++ test-estudando/src/app.test.tsx
@@ -1,9 +1,32 @@
 
+function sum(n1: number, n2: number){
+    return n1+n2
+}
+
+function media(n1: number, n2: number){
+
+    const resultado = (n1 + n2) / 2
+    if(resultado >= 6){
+        return "passou"
+    }
+    else{
+        return "reprovado"
+    }
+}
+
 //Criar um bloco que agrupo varios testese relacionados
 describe("First test app component", () => {
-    it("should add 1 + 2 to equal 3", () => {
-        expect(1).toBe(1);
+    
+    it("should add 5 + 2 to equal 7", () => {
+        expect(sum(5, 2)).toBe(7)
     })
+
+    it("Calculate the average and return whether you passed or failed", () => {
+        expect(media(6, 7)).toBe("passou")
+        expect(media(5, 5)).toBe("reprovado")
+    })
+
+
 })
 
 export default {}
